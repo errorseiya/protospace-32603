75888f78a9c81c48a81cb9bdee385d091ba4073c
errorseiya <errorseiya@gmail.com>
2020-12-27 12:24:34 +0900

    ログインページにリダイレクトされるように修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :set_prototype, only: [:show, :edit, :update, :destroy]
-  before_action :Identification_to_index, only: [:edit, :destroy, :update]
+  before_action :Identification_to_session, only: [:edit, :destroy, :update]
   before_action :authenticate_user!, except: [:index, :show]
   
   
@@ -56,9 +56,9 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
-  def Identification_to_index
+  def Identification_to_session
     unless current_user == @prototype.user
-      redirect_to action: :index
+      redirect_to new_user_session_path
     end
   end
 end
